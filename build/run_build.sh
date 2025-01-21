a2f7f62b37326a2d12c39fb049c93e18a9bbdf18
Rafael <rafael.r.mattos04@gmail.com>
2025-01-21 01:18:52 -0300

    Update /build

---
+++ build/run_build.sh
@@ -0,0 +1,5 @@
+# !/bin/bash
+# --- Ativar o ambiente virtual ---
+source ./.venv/bin/activate
+# --- Rodar o PyInstaller para gerar o executável ---
+pyinstaller --onefile --add-data "assets/ffmpeg:." --name "YoutubeDownloader" src/main.py
