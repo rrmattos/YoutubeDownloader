06e4d2b3ef97e504cbde08fb07ead38c116bda68
Rafael <rafael.r.mattos04@gmail.com>
2025-01-21 01:41:52 -0300

    Update /scripts

--- scripts/run_build.bat
+++ scripts/run_build.bat
@@ -1,6 +1,6 @@
 @echo off
 REM --- Ativar o ambiente virtual ---
-call .\.venv\Scripts\activate
+call .\venv\Scripts\activate
 REM --- Rodar o PyInstaller para gerar o executável ---
 PyInstaller --onefile --add-data "assets/ffmpeg;." --name "YoutubeDownloader" src\main.py
 pause
--- scripts/run_build.sh
+++ scripts/run_build.sh
@@ -1,5 +1,5 @@
 # !/bin/bash
 # --- Ativar o ambiente virtual ---
-source ./.venv/bin/activate
+source ./venv/bin/activate
 # --- Rodar o PyInstaller para gerar o executável ---
 pyinstaller --onefile --add-data "assets/ffmpeg:." --name "YoutubeDownloader" src/main.py
